6ff64052df0961211ff61e392c02dc4ab900d768
merge-simpson <letsdev@kakao.com>
2025-01-19 15:29:34 +0900

    first commit

---
+++ src/main/java/com/example/demo/Demo1stWeekSunApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1stWeekSunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1stWeekSunApplication.class, args);
+    }
+
+}
